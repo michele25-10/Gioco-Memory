00966d73d5300ea491b51941df7b4511ccaf72c4
michele25-10 <michele.gabri01@gmail.com>
2021-09-23 15:35:12 +0200

    Inserite copertine caselle

--- Memory_Michele_Gabrieli/Memory_Michele_Gabrieli/Properties/Resources.Designer.cs
+++ Memory_Michele_Gabrieli/Memory_Michele_Gabrieli/Properties/Resources.Designer.cs
@@ -63,6 +63,16 @@ namespace Memory_Michele_Gabrieli.Properties {
         /// <summary>
         ///   Cerca una risorsa localizzata di tipo System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap Coopertina {
+            get {
+                object obj = ResourceManager.GetObject("Coopertina", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Cerca una risorsa localizzata di tipo System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap Marvel_sfondo {
             get {
                 object obj = ResourceManager.GetObject("Marvel sfondo", resourceCulture);
